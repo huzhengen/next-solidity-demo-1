1cf7781627873b882126ed5b7dbcd16de7b9ae19
huzhengen <huzhengen@163.com>
2024-12-16 12:38:56 +0800

    add metamask card

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
+'use client'
 import Image from "next/image";
+import MetaMaskCard from "./components/connectorCards/MetaMaskCard";
+import YiDengTokenAbi from './abis/YiDengToken.json';
+import CourseMarketAbi from './abis/CourseMarket.json';
+import { hooks } from './connections/metaMask';
+import { Contract, ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+// import { InfoContract, InfoContract__factory } from '@/abis/types';
+const { useChainId, useAccounts, useIsActivating, useIsActive, useProvider, useENSNames } = hooks;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { useProvider } = hooks;
+  const provider = useProvider();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const signer = provider?.getSigner();
+  console.log('process.env.tokenAddress', process.env)
+  //找到合约地址
+  const yiDengTokenAddress = process.env.NEXT_PUBLIC_TOKEN_ADDRESS
+  const courseAddress = process.env.NEXT_PUBLIC_COURSE_ADDRESS
+  if (!yiDengTokenAddress || !courseAddress) {
+    return <div>合约地址不存在</div>
+  }
+
+  // const contract = InfoContract__factory.connect(address, signer!);
+  const yiDengTokenContract = new ethers.Contract(yiDengTokenAddress, YiDengTokenAbi, signer);
+  const courseContract = new ethers.Contract(courseAddress, CourseMarketAbi, signer);
+
+  const accounts = useAccounts();
+  console.log('accounts', accounts);
+
+  const [isPending, setPending] = useState(false);
+  const [ethValue, setEthValue] = useState(0);
+  const [ydValue, setYdValue] = useState(0);
+  const [balance, setBalance] = useState('')
+
+  const getBalance = async () => {
+    if (accounts && accounts.length > 0) {
+      console.log('getBalance');
+      const data = await yiDengTokenContract.balanceOf(accounts[0]);
+      console.log('balance', data);
+      setBalance(data.toString());
+    }
+  }
+
+  useEffect(() => {
+    if (accounts && accounts?.length > 0) {
+      getBalance()
+    }
+  }, [yiDengTokenAddress, yiDengTokenContract])
+
+  useEffect(() => {
+    if (!provider || !signer) return;
+
+    const handleEvent = (param1, param2, event) => {
+      try {
+        console.log('Event received:', { param1, param2, event });
+        getBalance()
+      } catch (error) {
+        console.error('Error handling event:', error);
+      }
+    };
+
+    try {
+      // contract.on('DepositMade', handleEvent);
+    } catch (error) {
+      console.error('Error setting up event listener:', error);
+    }
+
+    return () => {
+      try {
+        // contract.off('DepositMade', handleEvent);
+      } catch (error) {
+        console.error('Error removing event listener:', error);
+      }
+    };
+  }, [yiDengTokenContract])
+
+
+  useEffect(() => {
+    console.log('isPending: ', isPending);
+  }, [isPending]);
+
+  return (
+    <div className="">
+      <header className="flex justify-between items-center">
+        <h1>Next.js</h1>
+
+        <MetaMaskCard />
+      </header>
     </div>
   );
 }
